8fb3b414aea103b73bcfc32891b7ff4d3b18183d
uriapete <uriarteangelo@gmail.com>
2023-08-01 17:14:43 -0700

    start gameboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <h1 className="text-4xl">Matching Game!</h1>
+      <div className="gameboard flex-initial">
+        
+      </div>
     </main>
   )
 }
