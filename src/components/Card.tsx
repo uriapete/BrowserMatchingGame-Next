a660ac9c8c270e6f7a608c82b0e0203ba199e2e7
uriapete <uriarteangelo@gmail.com>
2023-08-02 18:21:26 -0700

    components/Card.tsx - begin styling for card comp

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps{
 export default function Card(props:CardProps){
     const{frontTxt}=props
     return(
-        <div className="comp comp-Card">
+        <div className="comp comp-Card bg-blue-400 text-white p-8 m-1">
             {frontTxt}
         </div>
     )
