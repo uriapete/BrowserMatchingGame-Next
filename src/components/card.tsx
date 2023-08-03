50c850489828e11002d06f27a413b853655664e6
uriapete <uriarteangelo@gmail.com>
2023-08-03 16:27:17 -0700

    components/card.tsx - temp remove flip fn

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,20 +4,24 @@ import { useState } from "react"
 interface CardProps{
     frontTxt:string
     backTxt?:string
+    flipped?:boolean
 }
 
 export default function Card(props:CardProps){
-    const{frontTxt}=props
+    const{frontTxt,flipped}=props
     let {backTxt}=props
     if(typeof backTxt==="undefined"){
         backTxt="Match!"
     }
-    const [flipped, setFlipped] = useState(false)
-    function handleFlip(){
-        setFlipped(!flipped)
-    }
+    // const [flipped, setFlipped] = useState(false)
+    // function handleFlip(){
+    //     setFlipped(!flipped)
+    // }
     return(
-        <div onClick={handleFlip} className={`comp comp-Card m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5 ${flipped ?"bg-white text-black ":"bg-blue-400 text-white "}`}>
+        <div
+            // onClick={handleFlip}
+            className={`comp comp-Card m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5 ${flipped ?"bg-white text-black ":"bg-blue-400 text-white "}`}
+        >
             {flipped?frontTxt:backTxt}
         </div>
     )
