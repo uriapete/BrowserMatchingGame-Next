4ca63413eaf5ce650a25bbcf2fafd79dd817821d
uriapete <uriarteangelo@gmail.com>
2023-08-03 14:59:09 -0700

    components/Card.tsx - further style card, set height, width, and center content

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,7 +6,7 @@ interface CardProps{
 export default function Card(props:CardProps){
     const{frontTxt}=props
     return(
-        <div className="comp comp-Card bg-blue-400 text-white p-8 m-1">
+        <div className="comp comp-Card bg-blue-400 text-white m-1 w-[7.5vw] h-[15vh] text-center flex flex-col justify-center">
             {frontTxt}
         </div>
     )
