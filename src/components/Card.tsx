bdd333f3e50c505b7e5a4684adaf774c4b60c2ae
uriapete <uriarteangelo@gmail.com>
2023-08-02 17:33:30 -0700

    components/Card.tsx - add props to card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,7 +1,13 @@
-export default function Card(){
+interface CardProps{
+    frontTxt:string
+
+}
+
+export default function Card(props:CardProps){
+    const{frontTxt}=props
     return(
         <div className="comp comp-Card">
-            
+            {frontTxt}
         </div>
     )
 }
