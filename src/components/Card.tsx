1d7d6423e5dac4e2ff9907eaf4e31f396afbf571
uriapete <uriarteangelo@gmail.com>
2023-08-03 15:58:35 -0700

    components/Card.tsx - begin flip function for cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,20 @@
+"use client"
+import { useState } from "react"
+
 interface CardProps{
     frontTxt:string
 }
 
 export default function Card(props:CardProps){
     const{frontTxt}=props
+    const backTxt="Match!"
+    const [flipped, setFlipped] = useState(false)
+    function handleFlip(){
+        setFlipped(!flipped)
+    }
     return(
-        <div className="comp comp-Card bg-blue-400 text-white m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5">
-            {frontTxt}
+        <div onClick={handleFlip} className={`comp comp-Card m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5 ${flipped ?"bg-white text-black ":"bg-blue-400 text-white "}`}>
+            {flipped?frontTxt:backTxt}
         </div>
     )
 }
