3398aa6b6345d2b977cbdb8b257fc76cd196acbf
uriapete <uriarteangelo@gmail.com>
2023-08-02 18:20:24 -0700

    Revert mislabeled "app/page.tsx - begin basic styling for card"
    This reverts commit 8541d2ca93c28b82aae1835ef253697d22a750fb, as wrong message was given for this commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Card from '@/components/Card'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-6">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <h1 className="text-4xl">Matching Game!</h1>
-      <div className="gameboard flex flex-row justify-center flex-initial container mx-auto w-1/4 flex-wrap">
-        <Card frontTxt='1' />
-        <Card frontTxt='1' />
-        <Card frontTxt='1' />
-        <Card frontTxt='1' />
-        <Card frontTxt='1' />
-        <Card frontTxt='1' />
+      <div className="gameboard flex-initial">
+        
       </div>
     </main>
   )
