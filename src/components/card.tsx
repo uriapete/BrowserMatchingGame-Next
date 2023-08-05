2267427f8593579c3f604e5ea81bc631cd96bf21
uriapete <uriarteangelo@gmail.com>
2023-08-04 20:34:39 -0700

    components/card.tsx - remove flipping logic from card component, as it will be moved to home page instead

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,25 +1,21 @@
-"use client"
-import { useState } from "react"
-
 interface CardProps{
     frontTxt:string
     backTxt?:string
     flipped?:boolean
+    onClick?:()=>any
 }
 
 export default function Card(props:CardProps){
-    const{frontTxt,flipped}=props
+    const{frontTxt,onClick}=props
     let {backTxt}=props
     if(typeof backTxt==="undefined"){
         backTxt="Match!"
     }
-    // const [flipped, setFlipped] = useState(false)
-    // function handleFlip(){
-    //     setFlipped(!flipped)
-    // }
+    
+    const{flipped}=props||false
     return(
         <div
-            // onClick={handleFlip}
+            onClick={onClick}
             className={`comp comp-Card m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5 ${flipped ?"bg-white text-black ":"bg-blue-400 text-white "}`}
         >
             {flipped?frontTxt:backTxt}
