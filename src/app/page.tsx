523700f677d8882b051dee5b56f28100670b98d7
uriapete <uriarteangelo@gmail.com>
2023-08-08 17:43:45 -0700

    app/page.tsx - implement card match checking

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import Card from '@/components/card'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-interface CardObj{
+interface ICard{
   frontTxt:string
 }
 
@@ -10,44 +10,68 @@ export default function Home(this: any) {
 
   const numCardsPerMatch=2
 
-  const cards:CardObj[]=[]
+  const cards:ICard[]=[]
+
+  let checkingMatch = false
 
   const [flippedCards, setFlippedCards] = useState<number[]>([])
 
   const [matchedCards, setMatchedCards] = useState<number[]>([])
 
   function flipCardToFrontSide(cardIdx: number) {
-    let flippedCardsLen=flippedCards.length
     setFlippedCards([...flippedCards, cardIdx])
-    flippedCardsLen++
-    return flippedCardsLen
   }
 
   function flipBack(){
     setFlippedCards([])
   }
 
-  // not complete, will implement match checking later
   function checkMatch(){
-    setTimeout(flipBack, 1000);
+    const matchTxt=cards[flippedCards[0]].frontTxt
+    for(let i=1;i<flippedCards.length;i++){
+      const elementTxt=cards[flippedCards[i]].frontTxt
+      if(elementTxt!==matchTxt){
+        return false
+      }
+    }
+    return true
   }
 
+  function confirmMatch(){
+    setMatchedCards([...matchedCards,...flippedCards])
+  }
+
+
   function handleCardFlip(cardIdx:number){
-    const numFlippedCards = flipCardToFrontSide(cardIdx)
-    console.log(numFlippedCards)
-    if(numFlippedCards>=numCardsPerMatch){
-      checkMatch()
-    }
+    flipCardToFrontSide(cardIdx)
   }
 
   for (let i = 0; i <= 3; i++) {
     for(let j=0;j<2;j++){
-      let newCard:CardObj={
+      let newCard:ICard={
         frontTxt:i.toString()
       }
       cards.push(newCard)
     }
   }
+
+  useEffect(() => {
+    function handleMatchCheck() {
+      if(flippedCards.length<numCardsPerMatch){
+        return null
+      }
+      checkingMatch=true
+      setTimeout(() => {
+        const chkMtch = checkMatch()
+        if (chkMtch) {
+          confirmMatch()
+        }
+        flipBack()
+      }, 750);
+    }
+    handleMatchCheck()
+  }, [flippedCards])
+
 
   return (
     <main className="flex min-h-screen flex-col items-center p-6">
@@ -55,14 +79,30 @@ export default function Home(this: any) {
       <div className="gameboardcontainer mx-auto grid grid-cols-3">
         {cards.map((card, idx) => {
           const{frontTxt}=card
-          let flipped = false
-          for (const flippedCard of flippedCards) {
-            if (idx === flippedCard) {
-              flipped = true
+          let matched = false
+          for(const matchedCard of matchedCards){
+            if(idx===matchedCard){
+              matched=true
               break
             }
           }
-          return <Card frontTxt={frontTxt} flipped={flipped} onClick={()=>{
+
+          let flipped = false
+          if(!matched){
+            for (const flippedCard of flippedCards) {
+              if (idx === flippedCard) {
+                flipped = true
+                break
+              }
+            }
+          }
+          return <Card frontTxt={frontTxt} matched={matched} flipped={flipped} onClick={()=>{
+            if(checkingMatch){
+              return null
+            }
+            if(matched||flipped){
+              return null
+            }
             handleCardFlip(idx)
           }} key={idx} />
         })}
