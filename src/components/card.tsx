fd013248e9f0110233fdcd767ba65199139fe94e
uriapete <uriarteangelo@gmail.com>
2023-08-08 17:40:28 -0700

    components/card.tsx - implement styling for matched cards

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,7 +1,10 @@
+import { match } from "assert"
+
 interface CardProps{
     frontTxt:string
     backTxt?:string
     flipped?:boolean
+    matched?:boolean
     onClick?:()=>any
 }
 
@@ -11,14 +14,23 @@ export default function Card(props:CardProps){
     if(typeof backTxt==="undefined"){
         backTxt="Match!"
     }
+
+    const{flipped,matched}=props||false
+
+    let displayClasses: string = "bg-blue-400 text-white "
+
+    if(matched){
+        displayClasses = "bg-gray-500 text-gray-100 "
+    }else if(flipped){
+        displayClasses = "bg-white text-black "
+    }
     
-    const{flipped}=props||false
     return(
         <div
             onClick={onClick}
-            className={`comp comp-Card m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5 ${flipped ?"bg-white text-black ":"bg-blue-400 text-white "}`}
+            className={`comp comp-Card m-1 w-[12vw] h-[24vh] text-center flex flex-col justify-center rounded border-white border-5 ${displayClasses}`}
         >
-            {flipped?frontTxt:backTxt}
+            {flipped||matched?frontTxt:backTxt}
         </div>
     )
 }
